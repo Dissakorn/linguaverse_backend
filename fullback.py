b63599f764c822a48b218464235d46cb86445afa
Dissakorn <dissakorn.p@hotmail.com>
2023-11-15 00:12:41 +0700

    Your commit message

--- fullback.py
+++ fullback.py
@@ -1,16 +1,34 @@
+from flask import Flask, request, jsonify
 import os
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 from pymongo import MongoClient
 
-# Load the credentials
+app = Flask(__name__)
+
+@app.route('/upload-photo', methods=['POST'])
+def upload_photo():
+    file = request.files['file']
+    if file:
+        filename = file.filename
+        temp_path = os.path.join('temp', filename)
+        file.save(temp_path)
+        
+        # Upload to Google Drive
+        file_url = upload_to_drive(temp_path, '1-m5NIqFtATKexE3KPCL376X0iC20LyR3')
+
+        # Save the file URL to MongoDB
+        save_url_to_mongo(file_url)
+
+        return jsonify({'message': 'Photo uploaded successfully!', 'file_url': file_url}), 200
+    return jsonify({'error': 'No file uploaded!'}), 400
+
 creds = None
 creds = service_account.Credentials.from_service_account_file(
-    r'C:\Users\User\Desktop\mecha\Neno\json\mecha-403710-e223c6e67d7b.json',
+    r'./json/mecha-403710-e223c6e67d7b.json',
     scopes=['https://www.googleapis.com/auth/drive']
 )
-
 # Build the drive service
 drive_service = build('drive', 'v3', credentials=creds)
 
@@ -42,14 +60,14 @@ def save_url_to_mongo(file_url):
     collection.insert_one(doc)
     client.close()
 
-def main():
+if __name__ == '__main__':
+    if not os.path.exists('temp'):
+        os.makedirs('temp')
     # Path to the file you want to upload
-    file_path = r'C:\Users\User\Desktop\mecha\Neno\temp\photo.png'
+    file_path = r'./temp/photo.png'
     folder_id = '1-m5NIqFtATKexE3KPCL376X0iC20LyR3'
     
     file_url = upload_to_drive(file_path, folder_id)  # Upload photo to Google Drive
-    
     save_url_to_mongo(file_url)  # Save the file URL to MongoDB
 
-if __name__ == '__main__':
-    main()
+    app.run(host='0.0.0.0', port=5000, debug=True)
